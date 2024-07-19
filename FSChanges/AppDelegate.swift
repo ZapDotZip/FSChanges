3da74602f6fc3cc3a339f9d5f7aa5c7310a7e7bd
zap.zip <43355748+ZapDotZip@users.noreply.github.com>
2024-07-19 16:19:14 -0700

    Updated quit handling

--- FSChanges/AppDelegate.swift
+++ FSChanges/AppDelegate.swift
@@ -73,56 +73,66 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		}
 	}
 	
-	func hasChanges() -> Bool {
-		return bgContext?.hasChanges ?? false
-	}
-	
 	func windowWillReturnUndoManager(window: NSWindow) -> UndoManager? {
 		// Returns the NSUndoManager for the application. In this case, the manager returned is that of the managed object context for the application.
 		return persistentContainer.viewContext.undoManager
 	}
 	
+	func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+		return !(bgContext?.hasChanges ?? false)
+	}
+	
 	func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
-		// Save changes in the application's managed object context before the application terminates.
-		let context = persistentContainer.viewContext
 		
-		if !context.commitEditing() {
+		if !bgContext!.commitEditing() {
 			NSLog("\(NSStringFromClass(type(of: self))) unable to commit editing to terminate")
 			return .terminateCancel
 		}
 		
-		if !context.hasChanges {
+		if !bgContext!.hasChanges {
 			return .terminateNow
 		}
 		
-		do {
-			try context.save()
-		} catch {
-			let nserror = error as NSError
-			
-			// Customize this code block to include application-specific recovery steps.
-			let result = sender.presentError(nserror)
-			if (result) {
-				return .terminateCancel
+		
+		
+		let alert = NSAlert()
+		alert.messageText = "Are you sure you want to quit?"
+		alert.informativeText = "Do you want to save the updated sizes? If you don't save, the next scan will reference the sizes from your last save. If you do save, the next scan will be compared against the most recent scan you did."
+		alert.alertStyle = .informational
+		alert.addButton(withTitle: "Save & Quit")
+		alert.addButton(withTitle: "Quit without saving")
+		alert.addButton(withTitle: "Cancel")
+		let res = alert.runModal()
+		if res == .alertFirstButtonReturn {
+			do {
+				try bgContext!.save()
+			} catch {
+				let nserror = error as NSError
+				
+				// Customize this code block to include application-specific recovery steps.
+				let result = sender.presentError(nserror)
+				if (result) {
+					return .terminateCancel
+				}
+				
+				let question = NSLocalizedString("Could not save changes while quitting. Quit anyway?", comment: "Quit without saves error question message")
+				let info = NSLocalizedString("Quitting now will lose any changes you have made since the last successful save", comment: "Quit without saves error question info");
+				let quitButton = NSLocalizedString("Quit anyway", comment: "Quit anyway button title")
+				let cancelButton = NSLocalizedString("Cancel", comment: "Cancel button title")
+				let alert = NSAlert()
+				alert.messageText = question
+				alert.informativeText = info
+				alert.addButton(withTitle: quitButton)
+				alert.addButton(withTitle: cancelButton)
+				
+				let answer = alert.runModal()
+				if answer == .alertSecondButtonReturn {
+					return .terminateCancel
+				}
 			}
-			
-			let question = NSLocalizedString("Could not save changes while quitting. Quit anyway?", comment: "Quit without saves error question message")
-			let info = NSLocalizedString("Quitting now will lose any changes you have made since the last successful save", comment: "Quit without saves error question info");
-			let quitButton = NSLocalizedString("Quit anyway", comment: "Quit anyway button title")
-			let cancelButton = NSLocalizedString("Cancel", comment: "Cancel button title")
-			let alert = NSAlert()
-			alert.messageText = question
-			alert.informativeText = info
-			alert.addButton(withTitle: quitButton)
-			alert.addButton(withTitle: cancelButton)
-			
-			let answer = alert.runModal()
-			if answer == .alertSecondButtonReturn {
-				return .terminateCancel
-			}
+		} else if res == .alertThirdButtonReturn {
+			return .terminateCancel
 		}
-		// If we got here, it is time to quit.
 		return .terminateNow
 	}
-	
 }
